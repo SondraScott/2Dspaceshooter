6cc18089fed31934b3904f3ffd8d527ce264723a
SondraScott <sondra.r.reeves@gmail.com>
2022-12-02 23:03:26 -0800

    created laser

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
     private float _speed = 3.5f;
-   
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    private float _nextFire = -1f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +21,17 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+
+        if(Input.GetKeyDown(KeyCode.Space)&& Time.time >  _nextFire)
+        {
+            _nextFire = Time.time + _fireRate;
+          Instantiate(_laserPrefab, transform.position + new Vector3(0,0.8f,0), Quaternion.identity);
+
+        }
+    }
+    void CalculateMovement()
+    {    
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput  = Input.GetAxis("Vertical");
 
